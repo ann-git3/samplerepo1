65a93ee7466db91f36313d0eb95edddd43dea053
91812 <91812@LAPTOP-QDIF68SG>
2026-05-01 12:01:40 +0530

    1/5/26

---
+++ samplemaven/src/main/java/exceptionhandling/ExceptionExample.java
@@ -0,0 +1,26 @@
+package exceptionhandling;
+public class ExceptionExample {
+public static void main(String[] args) {
+	
+	int a=10;
+	int b=0;
+	try {
+	int c=a/b;
+	System.out.println(c);
+	}
+	
+	/*catch(ArithmeticException d){
+		
+		 b=5;
+		int c=a/b;
+		System.out.println(c);
+		System.out.println(d);
+		
+	}*/
+	finally {
+		 b=5;
+			int c=a/b;
+			System.out.println(c);
+	}
+}
+}
